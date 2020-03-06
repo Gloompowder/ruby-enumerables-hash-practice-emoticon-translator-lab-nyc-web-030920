8a291180e72b1357ba67c7668e4320763c395b5d
William Lin <willlin1996@gmail.com>
2020-03-06 03:11:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,9 @@ def get_japanese_emoticon(file_path, emoticon_english)
   # code goes here
 new_variable = load_library(file_path)
 japanese_emoticon = new_variable[:get_emoticon][emoticon_english]
-if !
+if !japanese_emoticon 
+  return "Sorry, that emoticon was not found!"
+end
 end
 
 def get_english_meaning
