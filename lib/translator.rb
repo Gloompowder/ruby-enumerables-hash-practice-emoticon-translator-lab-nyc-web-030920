0744aa40e5ab7aba8cd7ea6f229d7b080151e8f1
William Lin <willlin1996@gmail.com>
2020-03-06 02:52:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  emoticons = YAML.load_file(file_path)
+  objects = YAML.load_file(file_path)
 end
 
 def get_english_meaning
