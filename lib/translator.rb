a775decbc7feea51749141c81b08670aa3c0004f
William Lin <willlin1996@gmail.com>
2020-03-06 03:02:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(file_path)
   hash
 end
 
-def get_japanese_emoticon(file_path, emoticon)
+def get_japanese_emoticon(file_path, emoticon_english)
   # code goes here
 new_variable = load_library(file_path)
 end
