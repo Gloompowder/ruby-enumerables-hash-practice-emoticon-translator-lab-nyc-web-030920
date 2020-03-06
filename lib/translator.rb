eb4d6911df59cb50f59eaedbefd6015852954213
William Lin <willlin1996@gmail.com>
2020-03-06 03:03:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 def get_japanese_emoticon(file_path, emoticon_english)
   # code goes here
 new_variable = load_library(file_path)
-hash
+new_variable[:get_emoticon]
 end
 
 def get_english_meaning
